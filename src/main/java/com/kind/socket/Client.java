2acbe3969af3df40acc580e9b3ba70f861ff2d66
周吉峰 <zhoujifeng@yunquna.com>
2019-12-02 20:08:34 +0800

    s

---
+++ src/main/java/com/kind/socket/Client.java
@@ -0,0 +1,26 @@
+package com.kind.socket;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * @author zhoujifeng
+ * @date 2019/12/2 20:01
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("客户端准备连接"+new Date());
+
+        try {
+            Socket socket = new Socket("localhost",8088);
+            System.out.println("客户端连接结束"+new Date());
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
